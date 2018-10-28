2d8e980c3f9da9245323beaa8e22d6adf19729da
Nathan Bradshaw <nathansbradshaw@gmail.com>
2018-10-27 19:06:27 -0500

    add navbar
    add navbar and page files for the site.

---
+++ src/pages/projects.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+import Layout from '../components/layout'
+
+const SecondPage = () => (
+  <Layout>
+    <h1>Projects</h1>
+    <p>This will be a blog type page showing off the past projects that the unitedCode has had, collectively, indivisual projects go on the profile of the maker</p>
+  </Layout>
+)
+
+export default SecondPage
